1cd7558919e65777aa7bde1e3eae951db74fc803
Biruk Moges <birukmoges8@gmail.com>
2025-06-14 21:00:23 +0300

    fix: downgrade @hookform/resolvers to version 4.1.3; update zod to version 3.24.2 in package.json and pnpm-lock.yaml; wrap LoginSection in Suspense in LoginPage

--- src/app/(public)/admin/page.tsx
+++ src/app/(public)/admin/page.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { LoginSection } from "@/sections/auth/LoginSection";
 
 export default function LoginPage() {
   return (
     <div className="py-4">
       <div className="flex min-h-screen items-center justify-center">
-        <LoginSection />
+        <Suspense>
+          <LoginSection />
+        </Suspense>
       </div>
     </div>
   );
