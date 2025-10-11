8a4c87a3c6db2b467921c5b530664b0eebd317d8
Biruk Moges <birukmoges8@gmail.com>
2025-10-12 00:13:21 +0300

    fix: remove unused ui

--- src/app/(public)/projects/[id]/page.tsx
+++ src/app/(public)/projects/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { Badge } from "@/components/ui/badge";
+// import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 // import { mockProjectsData } from "@/data/user/mockProjectsData";
 import { getProjects } from "@/lib/api/project";
